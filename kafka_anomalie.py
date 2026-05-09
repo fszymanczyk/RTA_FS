3855fcef2cf23f78b7f8c3b7866748508618c710
fszymanczyk <szymanczykfilip1@gmail.com>
2026-05-09 18:01:02 +0200

    kafka_anomalie.py

--- kafka_anomalie.py
+++ kafka_anomalie.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, from_json,  to_timestamp, window
+from pyspark.sql.functions import col, from_json, to_timestamp, window
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType
- 
+
 tx_schema = StructType([
     StructField("tx_id",     StringType()),
     StructField("user_id",   StringType()),
@@ -10,26 +10,33 @@ tx_schema = StructType([
     StructField("category",  StringType()),
     StructField("timestamp", StringType()),
 ])
- 
- 
+
 spark = (
     SparkSession.builder
     .appName("Lab4-Anomaly")
     .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0")
     .getOrCreate()
 )
-spark.sparkContext.setLogLevel("WARN")
- 
-kafka_raw = (spark.readStream.format("kafka").option("kafka.bootstrap.servers", "broker:9092").option("subscribe", "transactions")
-.load())
- 
-parsed = kafka_raw.select(
-    from_json(col("value").cast("string"), tx_schema).alias("tx")
-).select("tx.*").withColumn("timestamp", to_timestamp("timestamp"))
 
-anomalie = (
+spark.sparkContext.setLogLevel("WARN")
+
+kafka_raw = (
+    spark.readStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", "broker:9092")
+    .option("subscribe", "transactions")
+    .load()
+)
+
+parsed = (
+    kafka_raw
+    .select(from_json(col("value").cast("string"), tx_schema).alias("tx"))
+    .select("tx.*")
+    .withColumn("event_time", to_timestamp("timestamp"))
+)
+
+anomalies = (
     parsed
-    .withWatermark("event_time", "2 minutes")
     .groupBy(
         window(col("event_time"), "60 seconds"),
         col("user_id")
@@ -38,11 +45,23 @@ anomalie = (
     .filter(col("count") > 3)
 )
 
+anomalies = anomalies.dropDuplicates(["user_id", "window"])
+
+alerts_formatted = anomalies.selectExpr(
+    "concat('ANOMALIA: user_id=', user_id, "
+    "' | liczba transakcji=', count, "
+    "' | window_start=', window.start, "
+    "' | window_end=', window.end) as message"
+)
+
+def print_alerts(df, epoch_id):
+    for row in df.collect():
+        print(row["message"])
+
 q = (
-    anomalie.writeStream
-    .format("console")
+    alerts_formatted.writeStream
     .outputMode("update")
-    .option("truncate", False)
+    .foreachBatch(print_alerts)
     .start()
 )
 
